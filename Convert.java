c999a7ddc071f6f0a54acc5606a4bc828731037c
RiabkovaYana <123588181+RiabkovaYana@users.noreply.github.com>
2023-02-25 07:31:01 -1200

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,20 @@
+public class Convert {
+
+    public static void main(String[] args) {
+
+        int eur = 100;
+        double usd;
+        double param = 0.95;
+
+        System.out.println("Result = " + eur * param + " usd ");
+
+
+    }
+
+}
+
+
+
+
+
+
